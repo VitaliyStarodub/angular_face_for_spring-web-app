5b09ecffe550baeae4ea63a93072e8eb5f33a850
VitaliyStarodub <starodub.vital@gmail.com>
2018-12-07 13:30:53 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import {CategoryService} from './services/category.service';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import {FormsModule} from '@angular/forms';
+import {UserService} from './services/user.service';
+
+const appRouts: Routes = [
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'registration', component: RegistrationComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoriesComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    FormsModule,
+    RouterModule.forRoot(appRouts),
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [CategoryService, UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../model/user';
+import {UserService} from '../services/user.service';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  private user: User;
+  private isEmailValid: boolean;
+  private passwordsMatch: boolean;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.user = new User();
+  }
+
+  public createUser(): void {
+    console.log(this.user);
+  }
+
+  public verifyEmail(): void {
+    this.userService.getByEmail(this.user.email).subscribe(
+      resp => {
+        this.isEmailValid = false;
+      },
+      err => {
+        this.isEmailValid = true;
+      }
+    );
+  }
+  public comparePasswords(): void {
+    this.passwordsMatch = this.user.password === this.user.verifiedPassword;
+  }
+
+  public validateRegistrationForm(): boolean {
+    return this.isEmailValid === undefined
+      ? false
+      : this.passwordsMatch === undefined
+        ? false
+        : this.isEmailValid && this.passwordsMatch;
+  }
+}
